8462f5126ff2bb7ba28578c16d8dabf89761c333
JakeGConnor <jakegconnor@gmail.com>
2024-04-18 21:32:35 -0700

    Added clock and some styling

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,12 +1,23 @@
 import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Tab1.css';
 
-const clockIn = () => {
-  console.log('Button clicked!');
-};
-
 const Tab1: React.FC = () => {
+  const [currentDate, setCurrentDate] = useState<string>('');
+
+  const clockIn = () => {
+    const currtime = {currentDate};
+    console.log(currtime);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDate(new Date().toLocaleString());
+    }, 1000); // Update every second
+
+    return () => clearInterval(interval); // Cleanup on component unmount
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
@@ -14,13 +25,14 @@ const Tab1: React.FC = () => {
           <IonTitle>Time Clock</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent fullscreen id="T1B">
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Time Clock</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonButton className="clockIn" onClick={clockIn}>Clock In</IonButton>
+        <p>{currentDate}</p>
+        <IonButton className="clockIn" onClick={clockIn}>Clock In</IonButton> 
       </IonContent>
     </IonPage>
   );
