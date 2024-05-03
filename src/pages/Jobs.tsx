5a6007b2fbbe09fe620e642e3ac93cd0dcc6326e
JakeGConnor <jakegconnor@gmail.com>
2024-05-02 18:41:03 -0700

    New jobs working and going to database also added an allert if all items are not input

--- src/pages/Jobs.tsx
+++ src/pages/Jobs.tsx
@@ -1,34 +1,48 @@
-import { IonContent, IonFooter, IonHeader, IonPage, IonList, IonItem, IonTitle, IonToolbar, IonButton, IonPopover, IonInput } from '@ionic/react';
+import { IonContent, IonFooter, IonAlert, IonHeader, IonPage, IonList, IonItem, IonTitle, IonToolbar, IonButton, IonPopover, IonInput } from '@ionic/react';
 import React, { useState, useEffect } from 'react';
 import { addDoc, collection } from 'firebase/firestore';
 import { firestore } from '../firebase/firebaseConfig';
 import './Jobs.css';
 
 const Jobs: React.FC = () => {
+  const [showPopover, setShowPopover] = useState(false);
+  const [jobName, setJobName] = useState('');
+  const [jobNum, setJobNum] = useState('');
+  const [jobDesc, setJobDesc] = useState('');
+  const [showErrorAlert, setShowErrorAlert] = useState(false);
 
-  const[jobName, setJobName] = useState('');
-  const[jobNum, setJobNum] = useState('');
-  const[jobDesc, setJobDesc] = useState('');
+  const handleJobClick = () => {
+    if(showPopover == false){setShowPopover(true);}
+    else{setShowPopover(false);}
+  };
 
   const handleNewJob = () => {
     newJob(jobName, jobNum, jobDesc)
   }
 
-  const newJob = async(jobName: string, jobNum: string, jobDesc: string) => {
-    const holdJob = collection(firestore, 'jobs');
-
-    const jobDoc = {
-      name: jobName,
-      num: jobNum,
-      description: jobDesc
+  const newJob = async (jobName: string, jobNum: string, jobDesc: string) => {
+    if (jobName === '' || jobNum === '' || jobDesc === '') {
+      setShowErrorAlert(true);
+    } else {
+      const holdJob = collection(firestore, 'jobs');
+  
+      const jobDoc = {
+        name: jobName,
+        num: jobNum,
+        description: jobDesc,
+      };
+  
+      try {
+        await addDoc(holdJob, jobDoc);
+        setJobName('');
+        setJobNum('');
+        setJobDesc('');
+        setShowPopover(false);
+      } catch (error) {
+        console.error('Error: ', error);
+      }
     }
-
-    try {
-      await addDoc(holdJob, jobDoc);
-    } catch (error) {
-      console.error('Error: ', error);
-    }
-  }
+  };
 
   return (
     <IonPage>
@@ -38,41 +52,45 @@ const Jobs: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Jobs</IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <h1>Jobs</h1>
-        <IonFooter class = "Footer">
-          <IonButton id="NewJob" className="NewJob">New Job</IonButton>
-            <IonPopover trigger="NewJob" triggerAction='click' alignment='center'>
-              <IonContent class="NewJob-Block">
-                <h2>New Job</h2>
-                <IonList>
-                  <IonItem>
-                    <IonInput label="Enter Job Name" labelPlacement='stacked'
-                      value={jobName} onIonChange={(e) => setJobName(e.detail.value!)}>   
-                    </IonInput>
-                  </IonItem>
-                  <IonItem>
-                    <IonInput label="Enter Job Number" labelPlacement='stacked'
-                      value={jobNum} onIonChange={(e) => setJobNum(e.detail.value!)}>
-                    </IonInput>
-                  </IonItem>
-                  <IonItem>
-                    <IonInput label="Job Description" labelPlacement='stacked'
-                      value={jobDesc} onIonChange={(e) => setJobDesc(e.detail.value!)}>
-                    </IonInput>
-                  </IonItem>
-                  <IonItem>
-                    <IonButton onClick={handleNewJob} className="NewJob">Enter Job</IonButton>
-                  </IonItem>
-                </IonList>
-              </IonContent>
-            </IonPopover>
+        <IonFooter id="Footer">
+          <IonButton id="NewJob" className="NewJob" 
+              onClick={handleJobClick}>New Job</IonButton>
+              <IonPopover isOpen={showPopover} alignment='center'>
+                <IonContent class="NewJob-Block">
+                  <h2>New Job</h2>
+                  <IonList>
+                    <IonItem>
+                      <IonInput label="Enter Job Name" labelPlacement='stacked'
+                        value={jobName} onIonChange={(e) => setJobName(e.detail.value!)}>   
+                      </IonInput>
+                    </IonItem>
+                    <IonItem>
+                      <IonInput label="Enter Job Number" labelPlacement='stacked'
+                        value={jobNum} onIonChange={(e) => setJobNum(e.detail.value!)}>
+                      </IonInput>
+                    </IonItem>
+                    <IonItem>
+                      <IonInput label="Enter Description" labelPlacement='stacked'
+                        value={jobDesc} onIonChange={(e) => setJobDesc(e.detail.value!)}>
+                      </IonInput>
+                    </IonItem>
+                    <IonItem>
+                      <IonButton onClick={handleNewJob} className="NewJob" 
+                        id="NewJob">Enter Job</IonButton>
+                    </IonItem>
+                  </IonList>
+                </IonContent>
+              </IonPopover>
         </IonFooter>
       </IonContent>
+      <IonAlert
+        isOpen={showErrorAlert}
+        onDidDismiss={() => setShowErrorAlert(false)}
+        header="Error"
+        message="All fields are required"
+        buttons={['OK']}
+      />
     </IonPage>
   );
 };
